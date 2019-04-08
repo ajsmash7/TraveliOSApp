cb680e941bb4389baa58fd2afc70c643bc9ddb36
AJMac <ajmac@ajmacs-imac.local>
2019-04-08 22:50:36 +0100

    Initial Commit

---
+++ TravelWishListApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TravelWishListApp
+//
+//  Created by AJMac on 4/8/19.
+//  Copyright © 2019 AJMac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
